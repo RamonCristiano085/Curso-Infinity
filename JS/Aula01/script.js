d9af6272b3871a956ed9d978562b30b8c6eeac30
Ramon <118093412+RamonCristiano085@users.noreply.github.com>
2026-02-03 21:11:36 -0300

    Add files via upload

---
+++ JS/Aula01/script.js
@@ -0,0 +1,24 @@
+let numero1 = Number(prompt("Digite outro Número"))
+let numero2 = Number(prompt("Digite outro Número"))
+
+let resultado
+
+resultado = numero1
+resultado += numero2
+console.log("Adição", resultado)
+
+resultado = numero1
+resultado -= numero2
+console.log("Subtração", resultado)
+
+resultado = numero1
+resultado *= numero2
+console.log("Multiplicação", resultado)
+
+resultado = numero1
+resultado /= numero2
+console.log("Divisão", resultado)
+
+resultado = numero1
+resultado %= numero2
+console.log("Resto da divisão", resultado)
